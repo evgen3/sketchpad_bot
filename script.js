e59eda984a1fc0a37dcea6d8973c84fbd4039adc
Evgeniy Kuznetsov <zhenkuzne@gmail.com>
2022-04-17 21:40:52 +0300

    add canvas and send result

--- script.js
+++ script.js
@@ -23,3 +23,36 @@
         responseElement.innerText = await response.text()
     })
 })()
+
+{
+    const {WebApp} = window.Telegram
+    const {MainButton} = WebApp
+    const sketchpadElement = document.getElementById('sketchpad')
+    const sketchpad = new Sketchpad(sketchpadElement, {
+        line: {
+            color: 'black',
+            size: 5
+        }
+    });
+
+    MainButton.onClick(() => {
+        MainButton.showProgress()
+
+        sketchpad.canvas.toBlob(async (blob) => {
+            const formData = new FormData()
+
+            formData.append('initData', WebApp.initData);
+            formData.append('photo_url', blob);
+    
+            await fetch('https://dev2.woobla.su/', {
+                method: 'POST',
+                body: formData
+            })
+    
+            WebApp.close()
+        }, 'image/jpg')
+    })
+
+    MainButton.setText('Готово')
+    MainButton.show()
+}
