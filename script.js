3d9635c315a944ac89f667b5de993ca26b439145
Evgeniy Kuznetsov <zhenkuzne@gmail.com>
2022-04-17 21:57:27 +0300

    add button for debug in browser

--- script.js
+++ script.js
@@ -1,58 +1,49 @@
-(async () => {
-    const output = document.getElementById('output')
-    const submit = document.getElementById('submit')
-    const responseElement = document.getElementById('response')
-
-    output.value = window.Telegram.WebApp.initDataUnsafe.query_id
-
-    submit.addEventListener('click', async () => {
-        const response = await fetch('https://dev2.woobla.su/', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                initData: window.Telegram.WebApp.initData,
-                result: {
-                    type: 'photo',
-                    photo_url: 'https://dev2.woobla.su/1.jpg'
-                }
-            })
-        })
-
-        responseElement.innerText = await response.text()
-    })
-})()
-
 {
     const {WebApp} = window.Telegram
     const {MainButton} = WebApp
-    const sketchpadElement = document.getElementById('sketchpad')
-    const sketchpad = new Sketchpad(sketchpadElement, {
-        line: {
-            color: 'black',
-            size: 5
+
+    class MySketchpad {
+        constructor() {
+            this.initLibrary()
+            this.initButton()
         }
-    });
 
-    MainButton.onClick(() => {
-        MainButton.showProgress()
+        initLibrary() {
+            const element = document.getElementById('sketchpad');
 
-        sketchpad.canvas.toBlob(async (blob) => {
-            const formData = new FormData()
+            this.sketchpad = new Sketchpad(element, {
+                line: {
+                    color: 'black',
+                    size: 5
+                }
+            });
+        }
 
-            formData.append('initData', WebApp.initData);
-            formData.append('photo_url', blob);
-    
-            await fetch('https://dev2.woobla.su/', {
-                method: 'POST',
-                body: formData
-            })
-    
-            WebApp.close()
-        }, 'image/jpg')
-    })
+        initButton() {
+            MainButton.onClick(this.submitHandler.bind(this));
+            document.getElementById('submit').addEventListener('click', this.submitHandler.bind(this))
+            MainButton.setText('Готово');
+            MainButton.show();
+        }
 
-    MainButton.setText('Готово')
-    MainButton.show()
+        submitHandler() {
+            MainButton.showProgress()
+
+            this.sketchpad.canvas.toBlob(async (blob) => {
+                const formData = new FormData()
+
+                formData.append('initData', WebApp.initData);
+                formData.append('photo_url', blob);
+        
+                await fetch('https://dev2.woobla.su/', {
+                    method: 'POST',
+                    body: formData
+                })
+        
+                WebApp.close()
+            }, 'image/jpg')
+        }
+    }
+
+    new MySketchpad()
 }
