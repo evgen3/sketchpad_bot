feaa4deab7755a6ed3076020377abba4f0150247
Evgeniy Kuznetsov <zhenkuzne@gmail.com>
2022-04-17 20:21:59 +0300

    add request button

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
 (async () => {
     const output = document.getElementById('output')
+    const submit = document.getElementById('submit')
+    const responseElement = document.getElementById('response')
+
     output.value = window.Telegram.WebApp.initDataUnsafe.query_id
+
+    submit.addEventListener('click', async () => {
+        const response = await fetch('https://dev2.woobla.su/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                initData: window.Telegram.WebApp.initData,
+                result: {
+                    type: 'photo',
+                    photo_url: 'https://dev2.woobla.su/1.jpg'
+                }
+            })
+        })
+
+        responseElement.innerText = await response.text()
+    })
 })()
