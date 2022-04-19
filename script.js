8f684041ac312cf6764e5c14079563024b9160cc
Evgeniy Kuznetsov <zhenkuzne@gmail.com>
2022-04-20 00:39:20 +0300

    add debug panel

--- script.js
+++ script.js
@@ -1,13 +1,64 @@
 {
     const {WebApp} = window.Telegram
     const {MainButton} = WebApp
-    const isInTelegram = !WebApp.initData
+    const isDebug = !WebApp.initData
+
+    class MyMainButton {
+        constructor() {
+            this.element = document.createElement('button')
+            this.element.style.display = 'none'
+        }
+
+        onClick(callback) {
+            this.element.addEventListener('click', callback);
+            MainButton.onClick(callback);
+        }
+
+        setText(text) {
+            this.element.innerText = text;
+            MainButton.setText(text);
+        }
+
+        show() {
+            this.element.style.display = 'inline-block'
+            MainButton.show();
+        }
+
+        showProgress() {
+            this.element.innerText += ' (загрузка)'
+            MainButton.showProgress()
+        }
+    }
+
+    class DebugPanel {
+        constructor(mainButton) {
+            this.mainButton = mainButton
+            this.init()
+
+            if (isDebug) {
+                this.mount()
+            }
+        }
+
+        init() {
+            this.element = document.createElement('div')
+            this.element.classList.add('debug-panel')
+            this.element.append(this.mainButton.element)
+        }
+        
+        mount() {
+            document.body.append(this.element)
+        }
+    }
 
     class MySketchpad {
         constructor() {
+            this.mainButton = new MyMainButton()
+            this.debugPanel = new DebugPanel(this.mainButton)
+
             this.initLibrary();
             WebApp.ready();
-            setTimeout(this.initButton.bind(this), 3000)
+            setTimeout(this.initButton.bind(this), 2000)
         }
 
         initLibrary() {
@@ -22,15 +73,13 @@
         }
 
         initButton() {
-            MainButton.onClick(this.submitHandler.bind(this));
-            document.getElementById('submit').addEventListener('click', this.submitHandler.bind(this));
-
-            MainButton.setText('Готово');
-            MainButton.show();
+            this.mainButton.onClick(this.submitHandler.bind(this));
+            this.mainButton.setText('Готово');
+            this.mainButton.show();
         }
 
         submitHandler() {
-            MainButton.showProgress()
+            this.mainButton.showProgress()
 
             this.sketchpad.canvas.toBlob(async (blob) => {
                 const formData = new FormData()
